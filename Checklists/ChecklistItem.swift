11fb6cd9787b7107f788d75e88eefe7b8efdd330
Fadie Hannona <fadiehannona@gmail.com>
2014-12-16 17:12:43 +0000

    logic added for view controlelr to display data on table view

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Fadie on 16/12/2014.
+//  Copyright (c) 2014 Fadie. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -9,17 +9,111 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
+    
+//    This declares that items will hold an array of ChecklistItem objects
+//    but it doesnt actually create that array
+//    At this point, items does not have a value yet
+    
+    var items: [ChecklistItem]
+    
+//    This instantiates the array. Now items contains a valid array object,
+//    but the array has no ChecklistItem objects inside it yet.
+    
+    required init(coder aDecoder: NSCoder) {
+        items = [ChecklistItem]()
+        
+        let row0item = ChecklistItem()
+        row0item.text = "Walk the dog"
+        row0item.checked = false
+        items.append(row0item)
+        
+        let row1item = ChecklistItem()
+        row1item.text = "Brush My Teeth"
+        row1item.checked = true
+        items.append(row1item)
+        
+        let row2item = ChecklistItem()
+        row2item.text = "Learn iOS Development"
+        row2item.checked = true
+        items.append(row2item)
+        
+        let row3item = ChecklistItem()
+        row3item.text = "Footy practice"
+        row3item.checked = false
+        items.append(row3item)
+        
+        let row4item = ChecklistItem()
+        row4item.text = "Eat Ice Cream"
+        row4item.checked = true
+        items.append(row4item)
+        
+        super.init(coder: aDecoder)
+    }
+    
+    func configureCheckmarkForCell(cell: UITableViewCell, indexPath: NSIndexPath) {
+        
+        let item = items[indexPath.row]
+        
+        if item.checked {
+            cell.accessoryType = .Checkmark
+        } else {
+            cell.accessoryType = .None
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.rowHeight = 44
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+            let item = items[indexPath.row]
+            item.checked = !item.checked
+            
+            configureCheckmarkForCell(cell, indexPath: indexPath)
+        }
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem") as UITableViewCell
+        
+        let item = items[indexPath.row]
+        
+        let label = cell.viewWithTag(1000) as UILabel
+        label.text = item.text
+        
+        configureCheckmarkForCell(cell, indexPath: indexPath)
+        return cell
+    }
+    
+//    The two methods above are part of UITableView's data source protocol.
+//    The data source is the link between your data and the table view.
+//    The table view needs to know how many rows of data it has and how it should display rows.
+//    Say to the table view, "this view controller is now your data source."
+//    "ask if questions about the data anytime you feel like it."
+    
+//    Once hooked up to a data source (VC) the table view sends 'numberOfRowsInSection' message
+//    to find out how many rows there are.
+//    And when the table view needs to draw a particular row on the screen, it sends the
+//    "CellForRowAtIndexPath" message to ask the data source for a cell.
+    
+    
 
 }
 
