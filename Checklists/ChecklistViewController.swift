0048e3083baefb30238784ef48aa6e2a7c90cd09
Fadie Hannona <fadiehannona@gmail.com>
2014-12-16 18:08:41 +0000

    add button adds hardcoded items

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -16,6 +16,19 @@ class ChecklistViewController: UITableViewController {
     
     var items: [ChecklistItem]
     
+    @IBAction func addItem() {
+        let newRowIndex = items.count
+        
+        let item = ChecklistItem()
+        item.text = "I am a new row"
+        item.checked = false
+        items.append(item)
+        
+        let indexPath = NSIndexPath(forRow: newRowIndex, inSection: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
+    }
+    
 //    This instantiates the array. Now items contains a valid array object,
 //    but the array has no ChecklistItem objects inside it yet.
     
